446db6e08baad2849d93c3726dcab347d3bdfd16
Ariel <arielpiresdealmeida@gmail.com>
2022-02-17 16:08:54 -0300

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <img src="./icons/logo.png" alt="logo" />
+    </div>
+  )
+}
+
+export default Header
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      font-family: sans-serif;
+  }
+
+  html {
+      font-size: 10;
+  }
+
+  body {
+      background-color: ;
+  }
+`
